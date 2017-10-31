926dd439f61dd736579f13ea4dd851a7adb98ec7
lebronjames <798258843@qq.com>
2017-10-31 14:32:33 +0800

    20171031

---
+++ src/main/java/com/example/demo/SpringCloudRibbonHystrixConsumer1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRibbonHystrixConsumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRibbonHystrixConsumer1Application.class, args);
+	}
+}
